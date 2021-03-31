771305fb719d7850488efd6b5e02df89eabccefc
Damir Kurtaev <56434766+Amikuto@users.noreply.github.com>
2021-03-31 15:04:33 +0300

    Initial commit

---
+++ src/test/java/uni/ami/todoproject/TodoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.ami.todoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
